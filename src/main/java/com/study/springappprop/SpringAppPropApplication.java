61bd0e70b68626eb57ebef76bc97fb4c2323966d
Sagar Sonawane <sagar10483@gmail.com>
2018-04-12 16:26:20 +0530

    basis spring boot application using web and jpa module.

---
+++ src/main/java/com/study/springappprop/SpringAppPropApplication.java
@@ -0,0 +1,12 @@
+package com.study.springappprop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppPropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppPropApplication.class, args);
+	}
+}
---
+++ src/test/java/com/study/springappprop/SpringAppPropApplicationTests.java
@@ -0,0 +1,16 @@
+package com.study.springappprop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAppPropApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
